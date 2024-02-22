ec155a4aaf7742a0121ed700bd73293e79fb0e1d
0-kaito <tanabekaito50@gmail.com>
2024-02-22 16:43:45 +0900

    20240222_管理者サイトログイン機能の途中

---
+++ src/main/java/jp/co/internous/ecsite/controller/AdminController.java
@@ -0,0 +1,25 @@
+package jp.co.internous.ecsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.internous.ecsite.model.form.LoginForm;
+
+@Controller
+@RequestMapping("/ecsite/admin")
+public class AdminController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "admintop";
+	}
+
+	@PostMapping("/welcome")
+	public String welcome(LoginForm form, Model model) {
+		
+		return "welcome";
+	}
+	
+}

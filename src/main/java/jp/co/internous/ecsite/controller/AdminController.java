b4b8fcaa8934f10d5a5a3d6ae868bb13f1a72e29
0-kaito <tanabekaito50@gmail.com>
2024-02-27 16:44:31 +0900

    20240227_管理者サイトログイン機能の途中

--- src/main/java/jp/co/internous/ecsite/controller/AdminController.java
+++ src/main/java/jp/co/internous/ecsite/controller/AdminController.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jp.co.internous.ecsite.model.domain.MstGoods;
 import jp.co.internous.ecsite.model.domain.MstUser;
+import jp.co.internous.ecsite.model.form.GoodsForm;
 import jp.co.internous.ecsite.model.form.LoginForm;
 import jp.co.internous.ecsite.model.mapper.MstGoodsMapper;
 import jp.co.internous.ecsite.model.mapper.MstUserMapper;
+
 
 @Controller
 @RequestMapping("/ecsite/admin")
@@ -46,11 +50,47 @@ public class AdminController {
 
         List<MstGoods> goods = goodsMapper.findAll();
         model.addAttribute("userName", user.getUserName());
+        model.addAttribute("password", user.getPassword());
         model.addAttribute("goods", goods);
 
         return "welcome";
     }
+    
+    @PostMapping("/goodsMst") 
+    public String goodsMst( LoginForm f, Model m) { 
+        m.addAttribute("userName", f.getUserName()); 
+        m.addAttribute("password", f.getPassword()); 
+        return "goodsmst"; 
+    }
+
+    @PostMapping("/addGoods")
+    public String addGoods(GoodsForm goodsForm, LoginForm loginForm, Model m) {
+        m.addAttribute("userName", loginForm.getUserName());
+        m.addAttribute("password", loginForm.getPassword());
+        
+        MstGoods goods = new MstGoods();
+        goods.setGoodsName(goodsForm.getGoodsName());
+        goods.setPrice(goodsForm.getPrice());
+        
+        goodsMapper.insert(goods);
+        
+        return "forward:/ecsite/admin/welcome";
+    }
+    @ResponseBody
+    @PostMapping("/api/deleteGoods")
+    public String deleteApi(@RequestBody GoodsForm f, Model m) {
+        try {
+            goodsMapper.delteByID(f.getId()); 
+        } catch (IllegalArgumentException e) {
+            return "-1"; // 例外がキャッチされた場合は、「処理に失敗した」印として"-1"を返却している
+        }
+        
+        return "1"; // 例外が起きず、ここまで到達できれば「処理が成功した」印として"1"を返却している
+    }
+
+
 }
+
 
 
 

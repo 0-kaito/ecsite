79caa567d135f72e802e25af068576eab288985d
0-kaito <tanabekaito50@gmail.com>
2024-03-12 15:11:23 +0900

    20240312_ユーザー登録機能完成

--- src/main/java/jp/co/internous/ecsite/controller/IndexController.java
+++ src/main/java/jp/co/internous/ecsite/controller/IndexController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -86,7 +85,7 @@ public class IndexController {
 	
 	
 	
-	@GetMapping("/signup")
+	@RequestMapping("/signup")
 	public String signup() {
 		return "signup";
 	}

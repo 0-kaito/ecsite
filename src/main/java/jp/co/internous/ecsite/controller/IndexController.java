a1304af997bf612ffba4b296ff6605fc0ea1ec6d
0-kaito <tanabekaito50@gmail.com>
2024-03-12 13:33:29 +0900

    20240312_ユーザー登録機能完成

--- src/main/java/jp/co/internous/ecsite/controller/IndexController.java
+++ src/main/java/jp/co/internous/ecsite/controller/IndexController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -85,7 +86,7 @@ public class IndexController {
 	
 	
 	
-	@RequestMapping("/signup")
+	@GetMapping("/signup")
 	public String signup() {
 		return "signup";
 	}
@@ -115,10 +116,11 @@ public class IndexController {
 	    user.setUserName(userForm.getUserName());
 	    user.setFullName(userForm.getFullName());
 	    user.setPassword(userForm.getPassword());
+	    user.setIsAdmin(0);
 
 	    userMapper.insert(user);
 
-	    return "forward:/ecsite/";
+	    return "redirect:/ecsite/";
 	}
 
 }
